f68819c1cca23d0b7683e1814eb9e207d9873788
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-02-22 10:54:16 +0500

    Add files via upload

---
+++ Saylani_App/Constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+    FirebaseConfig:{
+        apiKey: "AIzaSyD_vJX1EsOfnEYO7eZ6r7mySaYYDfGaz4s",
+        authDomain: "smitfyp.firebaseapp.com",
+        databaseURL: "https://smitfyp.firebaseio.com",
+        projectId: "smitfyp",
+        storageBucket: "smitfyp.appspot.com",
+        messagingSenderId: "84347961166"
+    }
+}
