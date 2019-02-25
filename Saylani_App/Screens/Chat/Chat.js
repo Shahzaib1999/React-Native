7162bce9e1e965c48097c05c2ec398fd5d335759
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-02-25 19:32:12 +0500

    Add files via upload

--- Saylani_App/Screens/Chat/Chat.js
+++ Saylani_App/Screens/Chat/Chat.js
@@ -1,126 +1,235 @@
 import React, { Component } from "react";
-import { Dimensions, TextInput, ScrollView,
+import {
+    Dimensions, TextInput, ScrollView,
     View,
     StyleSheet, Button, AsyncStorage
 } from "react-native";
-import {Text} from "react-native-elements";
+import { Text, Icon } from "react-native-elements";
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import { ListItem } from 'react-native-elements'
 
-const {width,height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 class Chat extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = ({
             message: "",
-            mesag:[]
+            mesag: [],
+            list: [],
+            lis: false
         })
     }
 
-    async componentWillMount(){
+    async componentWillMount() {
 
+        const db = firebase.firestore();
         const user = await AsyncStorage.getItem('userToken');
-        this.setState({uid: user});
+        this.setState({ uid: user });
+
+        db.collection("chat").where(`${user}`, "==", true).onSnapshot(res => {
+
+            if (res.docs.length) {
+
+                res.docs.forEach(li => {
+
+                    if (li.data().recieverid != user) {
+
+                        var a = { id: li.id, name: li.data().recievername, id2: li.data().recieverid, offerid: li.data().offer}
+                        this.setState({ list: [...this.state.list, a], lis: true });
+                        
+                    } else {
+                        
+                        var a = { id: li.id, name: li.data().recievername, id2: li.data().senderid, offerid: li.data().offer}
+                        this.setState({ list: [...this.state.list, a], lis: true });
+
+                    }
+                })
+
+            }
+            else {
+
+            }
+
+        })
 
     }
 
-    send(){
-        const {message,id1,id2,offid} = this.state;
+    lsit(id){
+        debugger
+        const db = firebase.firestore();
+        const {uid} = this.state;
+
+        db.collection("chat").where(`${id}`, "==", true).onSnapshot(res => {
+
+            if (res.docs.length) {
+                this.setState({list: []})
+
+                res.docs.forEach(li => {
+
+                    if (li.data().recieverid != user) {
+
+                        var a = { id: li.id, name: li.data().recievername, id2: li.data().recieverid, offerid: li.data().offer}
+                        this.setState({ list: [...this.state.list, a], lis: true });
+                        
+                    } else {
+                        
+                        var a = { id: li.id, name: li.data().recievername, id2: li.data().senderid, offerid: li.data().offer}
+                        this.setState({ list: [...this.state.list, a], lis: true });
+
+                    }
+
+                })
+
+            }
+            else {
+
+            }
+
+        })
+    }
+
+    send() {
+        const { message, uid, id2, offid, sendername, recievername } = this.state;
         // var id1 = this.props.navigation.getParam('id');
         // var id2 = this.props.navigation.getParam("userid");
         var DateNew = new Date();
         var currentTime = DateNew.toLocaleTimeString();
-        
-        if (id1 && id2) {
+        debugger
+        if (uid && id2) {
             debugger
-            
+
             const db = firebase.firestore();
-            
-            db.collection("chat").where(`${id1}`,"==",true).where(`${id2}`,"==",true).where("offer","==",offid).onSnapshot(res =>{
+
+            db.collection("chat").where(`${uid}`, "==", true).where(`${id2}`, "==", true).where("offer", "==", offid).onSnapshot(res => {
 
                 if (res.docs.length) {
-                    
+
                     db.collection("chat").doc(res.docs[0].id).collection("messages").add({
                         message,
-                        sender: id1,
+                        sender: uid,
                         reciever: id2,
                         Time: currentTime,
-                        Date:new Date()
-                    }).then(() =>{
-                        this.setState({message: ""})
+                        Date: new Date()
+                    }).then(() => {
+                        this.setState({ message: "" })
                     })
 
                 } else {
-                    
+
                     db.collection("chat").add({
-        
-                        [id1]:true,
+
+                        [uid]: true,
                         [id2]: true,
-                        offer: offid
-        
-                    }).then(res =>{
-        
+                        offer: offid,
+                        sendername,
+                        recievername,
+                        recieverid: id2,
+                        senderid: uid
+
+                    }).then(res => {
+
                         db.collection("chat").doc(res.id).collection("messages").add({
                             message,
-                            sender: id1,
+                            sender: uid,
                             reciever: id2,
                             Time: currentTime,
-                            Date:new Date()
-                        }).then(() =>{
-                            this.setState({message: ""})
+                            Date: new Date()
+                        }).then(() => {
+                            this.setState({ message: "" })
                         })
-        
+
                     })
 
                 }
 
             })
 
-            
+
         }
     }
-    
+
+    sendMessage(id,id2,offer) {
+
+        // const {uid} = this.state;
+        const db = firebase.firestore();
+
+        // db.collection("chat").doc(id).onSnapshot(a =>{
+
+        //     if (li.data().recieverid != user) {
+
+        //         this.setState({ id2: a.data().recieverid });
+                
+        //     } else {
+                
+        //         this.setState({ id2: a.data().senderid});
+
+        //     }
+            
+        // })
+
+        db.collection("chat").doc(id).collection("messages").orderBy("Date").onSnapshot(res =>{
+
+            if(res.docs.length){
+                this.setState({mesag: []});
+
+                res.docs.forEach(msg =>{
+
+                    var a = { sender: msg.data().sender, reciever: msg.data().reciever, message: msg.data().message };
+                    this.setState({ mesag: [...this.state.mesag, a],id2, offid: offer, lis: false})
+
+                })
+
+            }
+
+        })
+
+    }
+
     render() {
 
-        const {mesag,uid} = this.state;
+        const { mesag, uid, message, lis, list } = this.state;
         var id1 = this.props.navigation.getParam('id');
-        var id2 = this.props.navigation.getParam("userid");
+        var id2 = this.props.navigation.getParam("recieverid");
         var offid = this.props.navigation.getParam("offer");
+        var sendername = this.props.navigation.getParam("sendername");
+        var recievername = this.props.navigation.getParam("recievername");
+        debugger
         const db = firebase.firestore();
 
         if (this.state.id1 != id1 && this.state.id2 != id2) {
-debugger
-        
-            db.collection("chat").where(`${id1}`,"==",true).where(`${id2}`,"==",true).where("offer","==",offid).onSnapshot(res =>{
-                if (res.docs.length) {
-                    
-                    db.collection("chat").doc(res.docs[0].id).collection("messages").orderBy("Date").onSnapshot(ms =>{
-                        if (ms.docs.length) {
-                            
-                            if (mesag.length != ms.docs.length) {
-                            this.setState({mesag: []})
-                                ms.forEach(msg =>{
 
-                                var a = {sender: msg.data().sender,reciever: msg.data().reciever,message:msg.data().message }
-                                
+
+            db.collection("chat").where(`${id1}`, "==", true).where(`${id2}`, "==", true).where("offer", "==", offid).onSnapshot(res => {
+                if (res.docs.length) {
+
+                    db.collection("chat").doc(res.docs[0].id).collection("messages").orderBy("Date").onSnapshot(ms => {
+                        if (ms.docs.length) {
+
+                            if (mesag.length != ms.docs.length) {
+                                this.setState({ mesag: [] })
+                                ms.forEach(msg => {
+
+                                    var a = { sender: msg.data().sender, reciever: msg.data().reciever, message: msg.data().message }
+
 
                                     this.setState({
-                                        id1,id2,offid,mesag: [...this.state.mesag,a]
+                                        id1, id2, offid, sendername, recievername, mesag: [...this.state.mesag, a], lis:false
                                     })
 
-                                    
+
                                 })
-                                
+
                             }
 
 
                         } else {
-                            
+
                             this.setState({
-                                id1,id2,offid
+                                id1, id2, offid, sendername, recievername
                             })
 
                         }
@@ -128,55 +237,82 @@ debugger
 
 
                 } else {
-                    
+
                     this.setState({
-                        id1,id2,offid
+                        id1, id2, offid, sendername, recievername
                     })
 
                 }
             })
-        
-    }
+
+        }
+
+
+
+
 
         return (
-            <View style={styles.container}>
-                <View style={styles.chat}>
-                    <View style={styles.head}>
-                        <Text h2 style={{color: "#fff"}}>Chat</Text>
+            <View style={{ flex: 1 }}>
+
+                {lis ?
+                    <View>
+                        {list.map((item, i) => (
+                            <ListItem
+                                containerStyle={{ height: 70, width: width, backgroundColor: '#F5F4F4', borderWidth: 1 }}
+                                key={i}
+                                title={item.name}
+                                titleStyle={{ fontSize: 18, fontWeight: '400' }}
+                                onPress={() => this.sendMessage(item.id,item.id2,item.offerid)}
+                                chevron={true}
+                            />
+                        ))}
                     </View>
-                    <ScrollView scrollEventThrottle={16}>
-                        <View style={styles.message}>
-
-                            {mesag.length > 0 ?
-                                <View>
-                                    {mesag.map((item, index) =>
-                                    <View key={index}>
-                                    {uid == item.sender ?
-
-                                        <Text style={styles.msg2}>{item.message}</Text>
-                                        :
-                                        <Text style={styles.msg1}>{item.message}</Text>
-                                    }
-                                        </View>
-
-                                    )}
+                    :
+                    <View style={styles.container}>
+                        <View style={styles.chat}>
+                            <View style={styles.head}>
+                                <View style={styles.icon}>
+                                    <Icon name="chevron-left" type='font-awesome' color='#fff' onPress={() =>this.lsit(uid)} />
                                 </View>
-                            :
-                            <View></View>
-                            }
+                                <View style={styles.chathead}>
+                                    <Text h2 style={{ color: "#fff"}}>Chat</Text>
+                                </View>
+                            </View>
+                            <ScrollView scrollEventThrottle={16}>
+                                <View style={styles.message}>
 
+                                    {mesag.length > 0 ?
+                                        <View>
+                                            {mesag.map((item, index) =>
+                                                <View key={index}>
+                                                    {uid == item.sender ?
+
+                                                        <Text style={styles.msg2}>{item.message}</Text>
+                                                        :
+                                                        <Text style={styles.msg1}>{item.message}</Text>
+                                                    }
+                                                </View>
+
+                                            )}
+                                        </View>
+                                        :
+                                        <View></View>
+                                    }
+
+                                </View>
+                            </ScrollView>
+                            <View style={styles.input}>
+                                <TextInput
+                                    style={{ height: 40, borderWidth: 3 }}
+                                    placeholder="Type your message!"
+                                    onChangeText={(message) => this.setState({ message })}
+                                    value={message}
+                                />
+                                <Button title='Send' onPress={() => this.send()} />
+                            </View>
                         </View>
-                    </ScrollView>
-                    <View style={styles.input}>
-                        <TextInput
-                            style={{ height: 40, borderWidth:3 }}
-                            placeholder="Type your message!"
-                            onChangeText={(message) => this.setState({ message })}
-                        />
-                        <Button title='Send' onPress={()=>this.send()}/>
                     </View>
-                </View>
-                <Text></Text>
+                }
             </View>
         );
     }
@@ -189,31 +325,48 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    chat:{
+    chat: {
         flex: 1,
-        width: width,        
+        width: width,
         height: height,
         borderWidth: 2,
         borderColor: "#007bff",
     },
-    head:{
+    head: {
+        display: 'flex',
+        // flex: 1,
+        flexDirection: "row",
         height: 70,
         backgroundColor: "#007bff",
-        justifyContent: 'center',
+        // justifyContent: 'space-evenly',
         alignItems: 'center',
     },
-    message:{
+    chathead: {
+        // flex: '1',
+        justifyContent: 'center',
+        // justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 140,
+        // alignSelf: "flex-end"
+    },
+    icon:{
+        // flex: 2,
+        // justifyContent: 'flex-start'
+        // marginRight: 280,
+    //     marginTop: 20,
+    },
+    message: {
 
     },
-    input:{
+    input: {
         flex: 1,
         height: 50,
         // borderWidth: 2,
-        flexDirection: "column", 
+        flexDirection: "column",
         justifyContent: "flex-end",
         backgroundColor: "#fff"
     },
-    msg1:{
+    msg1: {
         backgroundColor: "rgba(46, 135, 223, 0.45)",
         width: width * .5,
         fontWeight: '400',
@@ -225,7 +378,7 @@ const styles = StyleSheet.create({
         justifyContent: "flex-start"
         // #80CFE9
     },
-    msg2:{
+    msg2: {
         backgroundColor: "rgba(46, 135, 223, 0.45)",
         width: width * .5,
         fontWeight: '400',
