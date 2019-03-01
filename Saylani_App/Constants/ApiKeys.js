918f8969da527470aa56f21320ef9135cad775ca
Muhammad Shahzaib <mohdshahzaib24@gmail.com>
2019-03-01 17:37:26 +0500

    Update ApiKeys.js

--- Saylani_App/Constants/ApiKeys.js
+++ Saylani_App/Constants/ApiKeys.js
@@ -1,10 +1,10 @@
 export default {
     FirebaseConfig:{
-        apiKey: "AIzaSyD_vJX1EsOfnEYO7eZ6r7mySaYYDfGaz4s",
-        authDomain: "smitfyp.firebaseapp.com",
-        databaseURL: "https://smitfyp.firebaseio.com",
-        projectId: "smitfyp",
-        storageBucket: "smitfyp.appspot.com",
-        messagingSenderId: "84347961166"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: ""
     }
 }
